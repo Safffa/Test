a6a55b49d348b84659a22d072611e25f4ceb16c8
Safffa <55353625+Safffa@users.noreply.github.com>
2019-09-16 12:06:57 +0200

    Nye filer
    Filer i reposity

---
+++ JSopgave4.js
@@ -0,0 +1,11 @@
+function calculate (){
+	var t = widthBox.value;
+	var w = document.getElementById("widthBox").value;
+	var l = document.getElementById("lenghtBox").value;
+	var h = document.getElementById("heightBox").value;
+	w=parseFloat(w);
+	h=parseFloat(h);
+	l=parseFloat(l);
+	var result = w*l*h;
+	document.getElementById("result").innerHTML = result;
+}
